419c4f26a047b66e52f5c0b5419fa7eada440512
Indra Sigicharla <indhhra@gmail.com>
2020-10-13 09:08:38 +0530

    Create Class_avg.py
    Calculates the class average.

---
+++ Class_avg.py
@@ -0,0 +1,12 @@
+n = p = int(input("Enter the number of students in the class: "))
+sum = 0
+while n:
+    n -= 1
+    m = float(input("Enter the marks: "))
+    sum += m
+    if n == 0:
+        break
+    else:
+        pass
+avg = sum / p
+print(avg)
