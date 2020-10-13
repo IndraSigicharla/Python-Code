4a369a2a3e327f0f0ef15a693407b1a74c87a95c
Indra Sigicharla <indhhra@gmail.com>
2020-10-13 09:04:35 +0530

    Create batting_avg.py
    Calculates the batting average.

---
+++ batting_avg.py
@@ -0,0 +1,6 @@
+b1 = int(input("Enter the first score of the batsman: "))
+b2 = int(input("Enter the second score: "))
+b3 = int(input("Enter the third score: "))
+b4 = int(input("Enter the fourth score: "))
+avg = (b1 + b2 + b3 + b4)/4
+print("The batting average is :", avg)
