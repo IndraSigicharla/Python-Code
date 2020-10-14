3c59bd654d4edf2323cf52c82f4fbc4db2784339
Indra Sigicharla <indhhra@gmail.com>
2020-10-14 12:19:26 +0530

    Removed redundant lines.

--- Small_Code/Area_circle.py
+++ Small_Code/Area_circle.py
@@ -1,4 +1,3 @@
 r = float(input("Enter the radius of the circle in cm: "))
 a = 3.14*r**2
-o = "The area of the circle is {} cm^2."
-print(o.format(a, ".2f"))
+print(f"The area of the circle is {a:.2f} cm^2.")
