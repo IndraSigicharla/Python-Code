53a2409a898d792a23388f79c25afb2ec1f16385
Indhhra Sigicharla <indhhra@gmail.com>
2020-10-21 17:34:07 +0530

    Minor change made.

--- Small_Code/email_checker.py
+++ Small_Code/email_checker.py
@@ -1,10 +1,9 @@
 import re
 email = input("Enter an Email ID: ").lower()
-checker = r"^[a-z0-9]+[\._]?[a-z0-9]+@{1}\w+[.]\w+$"
+checker = r"^\w+[\._]?[a-z0-9]+@{1}\w+[.]\w+$"
 if re.search(checker,email):  
     print("Valid Email ID")  
     print(email) 
 else:  
     print("Invalid Email ID")   
     print(email)
-   
