620a53fcde05cd0df2287de702185bcfc38a5674
IndraSigicharla <indhhra@gmail.com>
2020-10-26 10:18:48 +0530

    Code to check common digits in input.

---
+++ Small_Code/same_digit.py
@@ -0,0 +1,13 @@
+n1 = input()[::-1]
+n2 = input()[::-1]
+y = 1
+for i in range(len(n1)):
+    if n1[i] == n2[i]:
+        if i == 0:
+            print("Same at 1's position")
+            y = 0
+        elif i in range(1, len(n1) + 1):
+            print(f"Same at {10**i}th position")
+            y = 0
+if n1[i] != n2[i] and y:
+    print("No digits are same")
