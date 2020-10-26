431f85cd89832e437ee83540c155ea41bc410dea
IndraSigicharla <indhhra@gmail.com>
2020-10-26 16:49:11 +0530

    Added comments for ease of understanding.

--- Code_For_Fun/Binary_shift.py
+++ Code_For_Fun/Binary_shift.py
@@ -1,9 +1,9 @@
 v = int(input())
 p = int(input())
-a = bin(v)
+a = bin(v)              # Converts integer to binary, and the next part slices the "0b" part off.
 a = a[2:]
-x = v // 2**(p - 1)
-y = v * 2**(p - 1)
+x = v // 2**(p - 1)     # Equivalent to right-shift of bits. (x >> y).
+y = v * 2**(p - 1)      # Equivalent to left-shift of bits. (x << y).
 if x & 1 == 1:
     print(x)
 else:
