2b111b4e9e376ef2181aefae1ebe7566eaa81c1f
IndraSigicharla <indhhra@gmail.com>
2020-10-28 02:21:44 +0530

    Fixed error that caused all conditionals to run.

--- Small_Code/region_checker.py
+++ Small_Code/region_checker.py
@@ -1,13 +1,13 @@
 name, age, gender, region = ([] for i in range(4))
 n = int(input("Enter the number of customers: "))
 for x in range(n):
-    name.append(input("Enter the name: "))
-    age.append(int(input("Enter the age: ")))
-    gender.append(input("Enter the gender: "))
-    region.append(input("Enter the region: "))
+    name.extend([input("Enter the name: ")])
+    age.extend([int(input("Enter the age: "))])
+    gender.extend([input("Enter the gender: ")])
+    region.extend([input("Enter the region: ")])
 y = input("Enter the region you want to search: ")
-for i in range(n):
-    if y == region[i]:
+for i, x in enumerate(region):
+    if y in x:
         print(f"Name: {name[i]}", f"Age: {age[i]}", f"Gender: {gender[i]}", f"Region: {region[i]}", sep="\n")
-    else:
+    elif y not in region:
         print(f"The region {y} is not in our data.")
