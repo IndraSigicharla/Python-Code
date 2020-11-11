00cf34d8a2f2f324ef3755c9b7e34010462e6496
IndraSigicharla <indhhra@gmail.com>
2020-11-12 02:11:52 +0530

    Changed a small condition.

--- Small_Code/browser_cost.py
+++ Small_Code/browser_cost.py
@@ -5,8 +5,6 @@ if h >= 7 and m > 0:
     exit("Invalid Entry")
 elif h >= 5:
     amt += 200 + (h-5) + m
-elif h < 5:
+else:
     amt = h*50 + m
-elif h == 7 and m == 0:
-    amt = 300
 print(f"The cost of browsing the web is: {amt}")
