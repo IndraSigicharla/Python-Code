49e9ff8d1e577df30514f577b6de16b50d26e435
IndraSigicharla <indhhra@gmail.com>
2020-12-20 12:23:04 +0530

    Code to find all points with minimum distance between them.

---
+++ Code_For_Fun/min_distance_points.py
@@ -0,0 +1,16 @@
+from itertools import combinations as c
+
+def pos(m):
+    return ((m[1][0] - m[0][0])**2 + (m[1][1] - m[0][1])**2)**0.5
+
+n = int(input())
+a = [tuple(int(input()) for _ in range(2)) for _ in range(n)]
+x = list(c(a, 2))
+y = list(map(pos, x))
+z = list(zip(x, y))
+ID = [i for i, j in enumerate(z) if j[-1] == min(y)]
+for i in ID:
+    print(*x[i], sep="\n")
+
+"""Given 'n' points in an X-Y plane , write an algorithm and the subsequent Python code to 
+determine the pair of points that are closer."""
