dc9da5b1581befaf72c72c3ebfa65d319eec4672
IndraSigicharla <indhhra@gmail.com>
2020-12-30 13:12:34 +0530

    Made code more efficient and smaller.

--- Code_For_Fun/rook_queen_pos.py
+++ Code_For_Fun/rook_queen_pos.py
@@ -1,27 +1,12 @@
-def cartesian(*lsts):
-    result = [[]]
-    for target in list(map(tuple, lsts)):
-        result = [x + [y] for x in result for y in target]
-    for product in result:
-        yield tuple(product)
-a = list(range(1, 9))
-rookRow, rookColumn = int(input()), int(input())
-queenRow, queenColumn = int(input()), int(input())
-def UDLR_moves(x, y):
-    c1 = set(cartesian([x], a))
-    c2 = set(cartesian(a, [y]))
-    return list(c1.union(c2))
-diag = [(x, y) for x, y in list(cartesian(a, a)) if y - x == queenColumn - queenRow or y + x == queenColumn + queenRow]
-move_set_rook = UDLR_moves(rookRow, rookColumn)
-move_set_queen = UDLR_moves(queenRow, queenColumn)
+a = [(x, y) for x in list(range(1, 9)) for y in list(range(1, 9))]
+rr, rc = int(input()), int(input())
+qr, qc = int(input()), int(input())
+ULDR_moves = lambda x, y : [i for i in a if (i[1] == y or i[0] == x) and i != (qr, qc)]
+diag = [i for i in a if (i[1] - i[0] == qc - qr or sum(i) == qc + qr) and i != (rr, rc)]
+move_set_rook, move_set_queen = ULDR_moves(rr, rc), ULDR_moves(qr, qc)
 move_set_queen.extend(diag)
 common = list(set(move_set_queen).intersection(set(move_set_rook)))
-if (rookRow, rookColumn) in common:
-    common.remove((rookRow, rookColumn))
-if (queenRow, queenColumn) in common:
-    common.remove((queenRow, queenColumn))
-for i in sorted(common):
-    print(i)
+print(*sorted(common), sep = "\n")
 
 """Given the position of a Rook and a queen in a chess board (8X8 board),
 write an algorithm and the subsequent Python code to determine the common positions where both rook and queen can be placed in the next move."""
